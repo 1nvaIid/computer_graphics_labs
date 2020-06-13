2f031c79b0a927a7b8d5cf7e762acb5bd73525dd
1nvaIid <60899975+1nvaIid@users.noreply.github.com>
2020-06-13 23:06:04 +0300

    smoothing fix

--- lab2/P5.cpp
+++ lab2/P5.cpp
@@ -71,19 +71,19 @@ void P5_image::draw_line(int brightness, double wd, double x0, double y0, double
 	{
 		int dx = abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
 		int dy = abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
-		int err = dx - dy, e2, x2, y2;                          
+		int err = dx - dy, e2, x2, y2;
 		float ed = dx + dy == 0 ? 1 : sqrt((float)dx * dx + (float)dy * dy);
 
-		for (wd = (wd + 1) / 2; ; ) {                                  
+		for (wd = (wd + 1) / 2; ; ) {
 			setPixell(x0, y0, max(0.0, brightness * (abs(err - dx + dy) / ed - wd + 1)));
 			e2 = err; x2 = x0;
-			if (2 * e2 >= -dx) {                                        
+			if (2 * e2 >= -dx) {
 				for (e2 += dy, y2 = y0; e2 < ed * wd && (y1 != y2 || dx > dy); e2 += dx)
 					setPixell(x0, y2 += sy, max(0.0, brightness * (abs(e2) / ed - wd + 1)));
 				if (x0 == x1) break;
 				e2 = err; err -= dy; x0 += sx;
 			}
-			if (2 * e2 <= dy) {                                        
+			if (2 * e2 <= dy) {
 				for (e2 = dx - e2; e2 < ed * wd && (x1 != x2 || dx < dy); e2 += dy)
 					setPixelr(x2 += sx, y0, max(0.0, brightness * (abs(e2) / ed - wd + 1)));
 				if (y0 == y1) break;
@@ -104,7 +104,7 @@ void P5_image::setPixel(int x, int y, double brightness) {
 
 void P5_image::setPixell(int x, int y, double brightness) {
 	try {
-		data[x][y] = (brightness / depth + data[x + 1][y] + data[x][y - 1]) / 3;
+		data[x][y] = (brightness / (depth * 2) + data[x + 1][y] + data[x][y - 1]) / 3;
 	}
 	catch (const std::exception&) {
 
@@ -113,7 +113,7 @@ void P5_image::setPixell(int x, int y, double brightness) {
 
 void P5_image::setPixelr(int x, int y, double brightness) {
 	try {
-		data[x][y] = (brightness / depth + data[x-1][y]+ data[x][y+1])/3;
+		data[x][y] = (brightness / (depth*2) + data[x - 1][y] + data[x][y + 1]) / 3;
 	}
 	catch (const std::exception&) {
 
